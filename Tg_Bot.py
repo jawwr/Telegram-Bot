0b40c22bc20c4bedecd4b2d8122dde82831a178e
Arseny <20mineslend14@gmail.com>
2021-11-01 18:13:35 +0500

    финальная версия

--- Tg_Bot.py
+++ Tg_Bot.py
@@ -5,6 +5,7 @@ import config
 
 bot = telebot.TeleBot(config.Token)
 
+#Стартовое сообщение
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 	markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
@@ -15,10 +16,12 @@ def send_welcome(message):
 	item4 = types.KeyboardButton("!1 неделя")
 	item5 = types.KeyboardButton("!2 неделя")
 	item6 = types.KeyboardButton("/help")
-	markup.add(item1,item2, item3, item6)
+	item7 = types.KeyboardButton("/завтра")
+	markup.add(item1,item2, item3, item6, item7)
 
-	bot.send_message(message.chat.id, "Привет, {0.first_name}, я бот-помощник с расписанием.[beta]\n Вот список моих команд:\n /help - узнать список команд\n !пара - узнать, какая сейчас пара.\n !расписание - узнать какое сегодня расписание\n !неделя - узнать все расписание на неделю\n".format(message.from_user), parse_mode = 'html', reply_markup=markup)
+	bot.send_message(message.chat.id, "Привет, {0.first_name}, я бот-помощник с расписанием.\n Вот список моих команд:\n /help - узнать список команд\n /пара - узнать, какая сейчас пара.\n /расписание - узнать какое сегодня расписание\n <b>/завтра</b> - узнать расписание на завтра\n /неделя - узнать все расписание на неделю\n".format(message.from_user), parse_mode = 'html', reply_markup=markup)
 
+#Сообщение со всеми командами
 @bot.message_handler(commands=['help'])
 def send_help(message):
 
@@ -30,11 +33,12 @@ def send_help(message):
 	item4 = types.KeyboardButton("!1 неделя")
 	item5 = types.KeyboardButton("!2 неделя")
 	item6 = types.KeyboardButton("/help")
-	markup.add(item1,item2, item3, item6)
+	item7 = types.KeyboardButton("/завтра")
+	markup.add(item1,item2, item3, item6, item7)
 
-	bot.send_message(message.chat.id, "<b>Вот список моих команд:\n   !пара</b> - узнать, какая сейчас пара.\n   <b>!расписание</b> - узнать какое сегодня расписание\n   <b>!неделя</b> - узнать все расписание на текущую неделю\n<b>     [ !1 неделя</b> - узнать расписание на первую неделю<b> ]\n     [ !2 неделя</b> - узнать расписание на первую неделю ]", parse_mode = "html", reply_markup=markup)
+	bot.send_message(message.chat.id, "<b>Вот список моих команд:\n   /пара</b> - узнать, какая сейчас пара.\n   <b>/расписание</b> - узнать какое сегодня расписание\n   <b>/завтра</b> - узнать расписание на завтра\n   <b>/неделя</b> - узнать все расписание на текущую неделю\n<b>     [ !1 неделя</b> - узнать расписание на первую неделю<b> ]\n     [ !2 неделя</b> - узнать расписание на первую неделю ]", parse_mode = "html", reply_markup=markup)
 
-
+#Сообщение с расписанием текущей пары
 @bot.message_handler(commands=['пара'])
 def send_lessons(message):
 	lesson = get_lessons.get_lessons()
@@ -51,31 +55,37 @@ def send_lessons(message):
 	else:
 		bot.send_message(message.chat.id, "Сейчас идет пара {0}\nНачало пары: {1}\nКонец пары: {2}\nАудитория: {3}".format(lesson.name, lesson.time_start, lesson.time_end,lesson.auditorium))
 
+#Сообщение с расписанием на сегодняшний день
 @bot.message_handler(commands=['расписание'])
 def send_schedule(message):
 	text = get_lessons.get_all_lessons_today()
 	bot.send_message(message.chat.id, "<b>Все пары на сегодня:</b>\n{0}".format(text), parse_mode = 'html')
 
+#Сообщение с расписанием на текущую неделю
 @bot.message_handler(commands=['неделя'])
 def send_week(message):
 	text = get_lessons.get_all_lessons_weeks("")
 	bot.send_message(message.chat.id, "<b>Все расписание на неделю:</b>\n{0}".format(text), parse_mode = 'html')
 
+#Сообщение с расписанием на первую неделю
 @bot.message_handler(commands=['1неделя'])
 def send_first_week(message):
 	text = get_lessons.get_all_lessons_weeks("Первая")
 	bot.send_message(message.chat.id, "<b>Все расписание на неделю:</b>\n{0}".format(text), parse_mode = 'html')
 
+#Сообщение с расписанием на вторую неделю
 @bot.message_handler(commands=['2неделя'])
 def send_second_week(message):
 	text = get_lessons.get_all_lessons_weeks("Вторая")
 	bot.send_message(message.chat.id, "<b>Все расписание на неделю:</b>\n{0}".format(text), parse_mode = 'html')
 
+#Сообщение с расписанием на завтра
 @bot.message_handler(commands=['завтра'])
 def send_lessons_tomorrow(message):
 	text = get_lessons.get_all_lessons_tomorrow()
 	bot.send_message(message.chat.id, "<b>Все пары на завтра:</b>\n{0}".format(text), parse_mode = 'html')
 
+#Обработка сообщений с командами в другом виде
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	if message.text == "!1 неделя" or message.text == "!1 Неделя" or message.text == "!1Неделя" or message.text == "!1неделя":
@@ -86,7 +96,5 @@ def echo_all(message):
 		send_schedule(message)
 	elif message.text == "!пара" or message.text == "!Пара":
 		send_lessons(message)
-	# else:
-	# 	bot.reply_to(message, "Я не знаю, что на это ответить")
 
 bot.infinity_polling()
--- get_date.py
+++ get_date.py
@@ -1,21 +1,32 @@
-from datetime import datetime,time, timedelta
-import calendar
+from datetime import datetime,time,timedelta
 
+#Массив с наименованием дней недели, для расписания на неделю
 day_name = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота']
 
+# Получене даты
 def get_date_now():
     return datetime.today()
 
+# Получене номера дня недели
 def get_day_number():
     return (datetime.today().weekday())
 
+#Метод установки времени
 def set_time(timeset):
     time_set = timeset.split(':')
     return time(int(time_set[0]), int(time_set[1])).strftime('%H:%M')
 
+# Получене текущего времени(+5 часов из-за разницы во времени с хостингом)
 def get_time_now():
-    return datetime.today().strftime("%H:%M")
+    time = datetime.today()
+    time += timedelta(seconds = 18000)
+    return time.strftime("%H:%M")
 
+# Получене текущего времени
+def get_time_now_normal():
+    return datetime.today().strftime('%H:%M')
+
+# Получене номера недели
 def get_week_number():
     datenow = datetime.now()
     datetimefirst = datetime(2021,8,29)
